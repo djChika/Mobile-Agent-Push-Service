3d8bffa8e44dc18f457538b548f83ea65a726d7b
Chika <djchikalkin@gmail.com>
2020-07-16 19:15:30 +0300

    fix dublicate send headers

--- src/controllers/SubscribersController.ts
+++ src/controllers/SubscribersController.ts
@@ -5,57 +5,53 @@ import SubscribersRepository from './../repository/Subscribers';
 
 class SubscribersController {
   static push = async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const { agentId, title, body, data } = req.body;
-      if (!title || !body || !agentId) {
-        return res.status(400).send({
-          error: 'AgentId, title or body missed.'
-        });
-      }
-
-      const subscribers = (await SubscribersRepository.findByAgentId(
-        agentId
-      )) as Notification_Subscribers[];
-
-      const { shippingErrors, usersNotRegistered } = await sendPushNotification(
-        subscribers,
-        {
-          title,
-          body,
-          data
-        }
-      );
-
-      return res.json({ shippingErrors, usersNotRegistered });
-    } catch (error) {
-      return res.sendStatus(500).send(error);
+    const { agentId, title, body, data } = req.body;
+    if (!title || !body || !agentId) {
+      return res.status(400).send({
+        error: 'AgentId, title or body missed.'
+      });
     }
+
+    const subscribers = (await SubscribersRepository.findByAgentId(
+      agentId
+    )) as Notification_Subscribers[];
+
+    return sendPushNotification(subscribers, {
+      title,
+      body,
+      data
+    })
+      .then(({ shippingErrors, usersNotRegistered }) => {
+        return res.json({ shippingErrors, usersNotRegistered });
+      })
+      .catch(error => {
+        console.log('SubscribersController -> error', error);
+        return res.status(500).send(error);
+      });
   };
 
   static pushAll = async (req: Request, res: Response): Promise<Response> => {
-    try {
-      const { title, body, data } = req.body;
-      if (!title || !body) {
-        return res.status(400).send({
-          error: 'title or body missed.'
-        });
-      }
-
-      const subscribers = (await SubscribersRepository.read()) as Notification_Subscribers[];
-
-      const { shippingErrors, usersNotRegistered } = await sendPushNotification(
-        subscribers,
-        {
-          title,
-          body,
-          data
-        }
-      );
-
-      return res.json({ shippingErrors, usersNotRegistered });
-    } catch (error) {
-      return res.sendStatus(500).send(error);
+    const { title, body, data } = req.body;
+    if (!title || !body) {
+      return res.status(400).send({
+        error: 'title or body missed.'
+      });
     }
+
+    const subscribers = (await SubscribersRepository.read()) as Notification_Subscribers[];
+
+    return sendPushNotification(subscribers, {
+      title,
+      body,
+      data
+    })
+      .then(({ shippingErrors, usersNotRegistered }) => {
+        return res.json({ shippingErrors, usersNotRegistered });
+      })
+      .catch(error => {
+        console.log('SubscribersController -> error', error);
+        return res.status(500).send(error);
+      });
   };
 }
 
